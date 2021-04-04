07c4950e0f098ccc468622b192c2cf3a02331244
iTrueStyler <rjcvfnbh100@gmail.com>
2021-04-04 20:10:56 +0300

    Add func to choose pizza type and size

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import Header from './components/Header.jsx';
-import {Home,Cart} from './pages';
-import {Route } from 'react-router-dom';
+import { Home, Cart } from './pages';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+
+
+
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
+
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path='/' component ={Home} exact/>
-        <Route path='/cart' component ={Cart} exact/>
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
+        <Route path="/cart" component={Cart} exact />
       </div>
     </div>
   );
 }
-
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Button from './Button';
 import Header from './Header';
 import Categories from './Categories';
 import SortPopup from './SortPopup';
+import PizzaBlock from './PizzaBlock';
 
 export {
     Button,
     Header,
     Categories,
-    SortPopup
+    SortPopup,
+    PizzaBlock
 };
